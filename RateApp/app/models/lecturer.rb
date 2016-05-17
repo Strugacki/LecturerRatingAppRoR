c7f52899756a2f1608cf276c661aa60881dcaaec
Michał Górski <dajminazwe@gmail.com>
2016-05-17 23:11:22 +0200

    Trying to add paperclip to project, issues on getting rails generate paperclip command work

--- RateApp/app/models/lecturer.rb
+++ RateApp/app/models/lecturer.rb
@@ -1,3 +1,5 @@
 class Lecturer < ActiveRecord::Base
     belongs_to :user
+      has_attached_file :image, styles: { medium: "400x600#" }
+  	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
